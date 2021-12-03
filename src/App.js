3bf4947cc589ea366a9e54a5d281168ce09a7d17
Shubham Garg <shbhmgarg@gmail.com>
2021-12-03 21:24:26 +0530

    context API

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import Navbar from "./components/layout/Navbar";
 import "./App.css";
-import Users from "./components/users/Users";
 import User from "./components/users/User";
-import Search from "./components/users/Search";
-import axios from "axios";
 import Alert from "./components/layout/Alert";
 import About from "./components/pages/About";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import GithubState from "./context/github/GithubState";
+import AlertState from "./context/alert/AlertState";
+import Home from "./components/pages/Home";
+import NotFound from "./components/pages/NotFound";
 
 const App = () => {
-
-  const [users, setUsers] = useState([]);
-  const[user, setUser] = useState({});
-  const [repos, setRepos] = useState([]);
-  const [alert, setAlert] = useState(null);
-  const [loading, setLoading] = useState(false);
-
   // async componentDidMount() {
   //   this.setState({ loading: true, alert: null });
 
@@ -26,96 +20,26 @@ const App = () => {
 
   //   this.setState({ users: data, loading: false });
   // }
-
-  const searchUsers = async (text) => {
-    setLoading(true);
-    setAlert(null);
-
-    const { data } = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id?${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-    
-    setLoading(false);
-    setUsers(data.items);
-  };
-
-  const getUser = async (username) => {
-    setLoading(true);
-    setAlert(null);
-
-    const { data } = await axios.get(
-      `https://api.github.com/users/${username}?client_id?${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-
-    setLoading(false);
-    setUser(data);
-  };
-
-  const getUserRepos = async (username) => {
-    setLoading(true);
-    setAlert(null);
-
-    const { data } = await axios.get(
-      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id?${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-
-    setLoading(false);
-    setRepos(data);
-  };
-
-  const clearUsers = () => {
-    setUsers([]);
-    setAlert(null);
-  };
-
-  const showAlert = (msg, text) => {
-    setAlert({ msg: msg, text: text });
-    setTimeout(() => setAlert(null), 5000);
-  };
-
-    
-
   return (
-    <Router>
-      <div className='App'>
-        <Navbar />
-        <div className='container'>
-          <Alert alert={alert} />
-          <Switch>
-            <Route
-              exact
-              path='/'
-              render={(props) => (
-                <>
-                  <Search
-                    searchUsers={searchUsers}
-                    clearUsers={clearUsers}
-                    showClear={users.length > 0 ? true : false}
-                    setAlert={showAlert}
-                  />
-                  <Users loading={loading} users={users} />
-                </>
-              )}
-            />
-            <Route path='/about' component={About} />
-            <Route
-              path='/user/:login'
-              render={(props) => (
-                <User
-                  {...props}
-                  getUser={getUser}
-                  getUserRepos={getUserRepos}
-                  user={user}
-                  repos={repos}
-                  loading={loading}
-                />
-              )}
-            />
-          </Switch>
-        </div>
-      </div>
-    </Router>
+    <GithubState>
+      <AlertState>
+        <Router>
+          <div className='App'>
+            <Navbar />
+            <div className='container'>
+              <Alert />
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route path='/about' component={About} />
+                <Route path='/user/:login' component={User} />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </AlertState>
+    </GithubState>
   );
-}
+};
 
 export default App;
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,7 +1,13 @@
-import React, { useState } from "react";
-import PropTypes from "prop-types";
+import React, { useState, useContext } from "react";
+import GithubContext from "../../context/github/githubContext";
+import AlertContext from "../../context/alert/alertContext";
 
-const Search = ({ showClear, setAlert, clearUsers, searchUsers }) => {
+const Search = () => {
+  const githubContext = useContext(GithubContext);
+  const { searchUsers, clearUsers, users } = githubContext;
+
+  const alertContext = useContext(AlertContext);
+  const { setAlert } = alertContext;
 
   const [text, setText] = useState('');
 
@@ -14,12 +20,6 @@ const Search = ({ showClear, setAlert, clearUsers, searchUsers }) => {
     } else {
       searchUsers(text);
     }
-  };
-
-  const clear = (e) => {
-    e.preventDefault();
-    setText('');
-    clearUsers();
   };
 
   return (
@@ -38,20 +38,13 @@ const Search = ({ showClear, setAlert, clearUsers, searchUsers }) => {
           className='btn btn-dark btn-block'
         />
       </form>
-      {showClear && (
-        <button className='btn btn-block btn-light' onClick={clear}>
+      {users.length > 0 && (
+        <button className='btn btn-block btn-light' onClick={clearUsers}>
           Clear
         </button>
       )}
     </div>
   );
-};
-
-Search.propTypes = {
-  searchUsers: PropTypes.func.isRequired,
-  clearUsers: PropTypes.func.isRequired,
-  showClear: PropTypes.bool.isRequired,
-  setAlert: PropTypes.func.isRequired,
 };
 
 export default Search;
